f6d42981eef6a23afb908fb845a4a68bf138b064
e2dubba <svirgilgoodwin@gmail.com>
2023-09-09 12:08:06 -0600

    Update Main
    * adding pretty print
    * adding prefix finding
    * update printing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use oxigraph::{io::DatasetFormat, store::Store, sparql::{ QueryResults, QueryResultsFormat}};
+use oxigraph::{io::DatasetFormat, store::Store, sparql::QueryResultsFormat};
 use std::{fs, path::PathBuf, io::Cursor};
 use clap::Parser;
+use serde_json::Map;
+use serde_derive::Deserialize;
+#[macro_use] extern crate prettytable;
+use prettytable::{ Table, Row, Cell };
+
+
+mod prefix;
+use crate::prefix::{Prefix, find_prefixes};
+
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None )]
@@ -9,22 +18,23 @@ struct Args {
     #[arg(short, long)]
     directory: String,
 
-    /// name of the file for loading the query
+    /// name of the file or string for loading the query
     #[arg(short, long)]
     query: String,
 }
 
 
-fn update_store(store: &mut Store, path: PathBuf) -> Option<()> {
+fn update_store(store: &mut Store, path: PathBuf, ns_dict: &mut Prefix) -> Option<()> {
     let ext = path.extension()?;
     if ext.is_empty() { return None}
-    let file = fs::read(path); // read_to_string(path);
+    let file = fs::read(path);
 
     if file.is_err() {
         return None;
     }
 
     let file_contents = file.unwrap();
+    find_prefixes(&file_contents, ns_dict);
     let res = store.load_dataset(Cursor::new(&file_contents), DatasetFormat::TriG, None);
     if res.is_err() {
         println!("Error saving quads to store");
@@ -34,6 +44,65 @@ fn update_store(store: &mut Store, path: PathBuf) -> Option<()> {
     Some(())
 }
 
+#[derive(Deserialize)]
+struct SparqlJson {
+    head: HeadJson,
+    results: ResultJson,
+}
+
+#[derive(Deserialize)]
+struct HeadJson {
+    vars: Vec<Box<str>>,
+}
+
+#[derive(Deserialize)]
+struct ResultJson {
+    bindings: Vec<Map<String, serde_json::Value>>
+}
+
+
+fn print_query(store: &Store, query: &str, ns_dict: &mut Prefix) {
+    let mut writer: Vec<_> = Vec::new();
+    let solutions = store.query(query);
+
+    let res = solutions.unwrap().write(&mut writer, QueryResultsFormat::Json);
+    if res.is_err() {
+        println!("Error in parsing the results")
+    }
+    let object: SparqlJson = serde_json::from_slice(&writer).expect("Error in Parsing Json");
+    let vars = object.head;
+    let bindings: ResultJson = object.results;
+
+    let mut table = Table::new();
+    let headings = Row::new(vars.vars.clone().into_iter().map(|x| Cell::new(&x)).collect());
+    table.add_row(headings);
+
+    // the following loop should really be placed in its own function
+    // perhaps a module and re-write the pretty printing of the table
+    for result in bindings.bindings {
+        let mut print_res: Vec<Cell> = vec![];
+        for var in &vars.vars {
+            let var_map: &serde_json::Value = &result[&var.to_string()];
+            let rdf_type = &var_map["type"];
+            let let_return_value = match rdf_type.as_str() {
+                Some("uri") => {
+                    let res = ns_dict.shorten_uri(&var_map["value"].to_string());
+                    res
+                },
+                Some("literal") => var_map["value"].to_string(),
+                Some("bnode") => var_map["value"].to_string(),
+                Some("triple") => format!("{}\t{}\t{}", var_map["subject"], var_map["predicate"], var_map["object"]),
+                _ => continue
+            };
+            print_res.push(Cell::new(&let_return_value));
+        }
+        table.add_row(Row::new(print_res));
+
+    }
+    table.printstd();
+
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -41,15 +110,16 @@ fn main() {
 
     let dir = args.directory.clone();
 
+    let mut ns_dict: Prefix = Prefix::new();
+
     let paths = fs::read_dir(dir).unwrap();
     for path in paths {
         if path.is_err() {
             println!("Path contains error: {:?}", path);
             continue
         };
-        update_store(&mut store, path.unwrap().path());
+        update_store(&mut store, path.unwrap().path(), &mut ns_dict);
     }
-
 
     let length = store.len();
     if length.is_err() || length.unwrap() == 0 {
@@ -65,25 +135,12 @@ fn main() {
             println!("There is an error in reading the query file");
             return
         }
-        if let QueryResults::Solutions(solutions) = store.query(&read_file.unwrap()).unwrap() {
-            for solution in solutions {
-                println!("{:?}", solution.unwrap())
-            }
-        }
+        print_query(&store, &read_file.unwrap(), &mut ns_dict);
+
         return
     }
-    println!("query: {query}");
-    let solutions = store.query(&query);
+    // println!("query: {query}");
 
-    let mut writer: Vec<_> = Vec::new();
-    if solutions.is_err() {
-        println!("Error in evaluating query");
+    print_query(&store, &query, &mut ns_dict);
 
-    } else {
-        let res = solutions.unwrap().write(&mut writer, QueryResultsFormat::Tsv);
-        if res.is_err() {
-            println!("Error in parsing the results")
-        }
-        println!("{}", std::str::from_utf8(&writer).unwrap());
-    }
 }
